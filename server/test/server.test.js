0cec20cf5807d40dddbc8952913dea94e40a4062
Chad Griffis <cmg123@me.com>
2017-02-06 16:07:17 -0600

    adds additional express server testing

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -10,6 +10,7 @@ Default Timeout has been changed, but it is set back after test runs.
 The remaining tests here are to just be sure that the server is running
 correctly by checking that index.html is being served along with favicon
 */
+
 describe('builds application', () => {
   let originalTimeout;
   beforeEach(() => {
@@ -21,5 +22,30 @@ describe('builds application', () => {
   });
   afterEach(() => {
     jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+  });
+});
+
+describe('express serving', () => {
+  test('responds to / with the index.html', () => {
+    return request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .then(res => expect(res.text).to.contain('<div id="root"></div>'));
+  });
+
+  test('responds to favicon.icon request', () => {
+    return request(app)
+      .get('/favicon.ico')
+      .expect('Content-Type', 'image/x-icon')
+      .expect(200);
+  });
+
+  test('responds to any route with the index.html', () => {
+    return request(app)
+      .get('/foo/bar')
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .then(res => expect(res.text).to.contain('<div id="root"></div>'));
   });
 });
