19fc7c2d7ab09c765d256954f59aa09a605fdbb0
Chad Griffis <cmg123@me.com>
2017-02-14 17:24:31 -0600

    adds static endpoint for production, updates proxie settings and README

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # blogbuilder
 
-This application started with the intent to develop a blog management tool using react, redux,
-express, graphql, apollo, mongoDB.
+This application started with the intent to develop a blog management tool using react, redux, express, graphql, apollo, mongoDB.
+
+This repository is using concurrently to run server and client.  In development, Webpack is used for a proxied GraphQL server.
 
 ###Getting Started###
 
--- server/index.js
+++ server/index.js
@@ -8,11 +8,18 @@ const PORT = process.env.PORT || 9000;
 
 const app = express();
 
+// the GraphQL endpoint
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: grqlSchema }));
 
+// Graphical interactive in-browser GraphQL IDE located at this url
 app.use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
+
+// Serving the build files when in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
